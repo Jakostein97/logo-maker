d346e72d8f474beba1816a185df5efb3c6aa2e52
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-06-24 22:36:09 -0400

    Adds Shapes class

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const generateShapes = require('./lib/shapes.js');
 
+class Shapes {
+    constructor(triangle, circle, square) {
+        this.triangle = triangle;
+        this.circle = circle;
+        this.square = square;
+    }
+}
+
