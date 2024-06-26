cae8021085c25d0b77c582abc2aaf29237dd3821
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-06-25 20:46:55 -0400

    Adds the object classes

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const generateShapes = require('./lib/shapes.js');
+const { type } = require('os');
+const { default: Choices } = require('inquirer/lib/objects/choices.js');
 
-class Shapes {
-    constructor(triangle, circle, square) {
-        this.triangle = triangle;
-        this.circle = circle;
-        this.square = square;
-    }
-}
-
+inquirer
+.prompt ([
+    {
+        type: 'input',
+        name: "text",
+        message: 'Enter up to three characters',
+        default: "JAF",
+    },
+    {
+        type: 'input',
+        name: "textColor",
+        message:  "Enter a color keyword for the text color",
+        default: 'white',
+    },
+    {
+        type: 'list',
+        name: "shape",
+        message:  "Choose a shape",
+        choices: ['triangle', 'square', 'circle'],
+        default: 'square',
+    },   
+    {
+        type: 'input',
+        name: "shapeColor",
+        message:  "Enter a color keyword for the shape color",
+        default: 'black',
+    },
+])
+.then((responses) => {
+    console.log(responses);
+    fs.writeFile('logo.svg', JSON.stringify(responses))
+    .then(() => {
+        console.log('Generated logo.svg');
+    })
+})
